897b1ff10d568f3b047a9bdeab5cd94064992da5
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-23 16:02:27 +0200

    Create UI for the game and add style to it
    Reworked the project to provide buttons for the user to select their
    input for the game. All information is displayed instead of console.log.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 let humanScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
+const maxRoundCount = 5;
 
 function getComputerChoice() {
-    let randomNumber = Math.random();
     let computerChoice;
+    const pcChoice = document.querySelector("#pc-choice"); 
+    let randomNumber = Math.random();
     if (randomNumber < 0.33) {
         computerChoice = "rock";
     }
@@ -14,37 +16,42 @@ function getComputerChoice() {
     else {
         computerChoice = "scissors";
     }
-    console.log(`Computer's choice: ${computerChoice}`);
+    pcChoice.textContent = `Computer's choice: ${computerChoice}`;
     return computerChoice;
 }
 
-function getHumanChoice() {
-    const message = "Input rock, paper or scissors";
-    let userChoice;
-    while (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
-        userChoice = prompt(message).toLowerCase();
+function getHumanChoice(humanChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    const userChoice = document.querySelector("#user-choice"); 
+    if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        alert("Please choose rock, paper of scissors!");
+        return;
     }
-    console.log(`User's choice: ${userChoice}`);
-    return userChoice;
+    userChoice.textContent = `User's choice: ${humanChoice}`;
+    return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
+    if (roundsPlayed >= maxRoundCount) {
+        return;
+    }
     let result;
+    const roundResult = document.querySelector("#round-result"); 
     switch (humanChoice) {
         case "rock":
           switch (computerChoice) {
             case "rock":
-                result = "Rock vs. Rock. It's a tie";
+                roundResult.textContent = "Rock vs. Rock. It's a tie";
                 break;
 
             case "paper":
                 computerScore++;
-                result = "You lose! Paper beats Rock";
+                roundResult.textContent = "You lose! Paper beats Rock";
                 break;
 
             case "scissors":
                 humanScore++;
-                result = "You win! Rock beats Scissors";
+                roundResult.textContent = "You win! Rock beats Scissors";
           }
           break;
       
@@ -52,16 +59,16 @@ function playRound(humanChoice, computerChoice) {
             switch (computerChoice) {
                 case "rock":
                     humanScore++;
-                    result = "You win! Paper beats Rock";
+                    roundResult.textContent = "You win! Paper beats Rock";
                     break;
     
                 case "paper":
-                    result = "Paper vs. Paper. It's a tie";
+                    roundResult.textContent = "Paper vs. Paper. It's a tie";
                     break;
     
                 case "scissors":
                     computerScore++;
-                    result = "You lose! Scissors beats Paper";
+                    roundResult.textContent = "You lose! Scissors beat Paper";
             }
             break;
         
@@ -69,38 +76,59 @@ function playRound(humanChoice, computerChoice) {
             switch (computerChoice) {
                 case "rock":
                     computerScore++;
-                    result = "You lose! Rock beats Scissors";
+                    roundResult.textContent = "You lose! Rock beats Scissors";
                     break;
     
                 case "paper":
                     humanScore++;
-                    result = "You win! Scissors beats Paper";
+                    roundResult.textContent = "You win! Scissors beat Paper";
                     break;
     
                 case "scissors":
-                    result = "Scissors vs. Scissors. It's a tie!";
+                    roundResult.textContent = "Scissors vs. Scissors. It's a tie!";
             }
     }
-    console.log(result);
-    console.log(`Your score: ${humanScore}`);
-    console.log(`PC score: ${computerScore}`);    
     roundsPlayed++;
-    return result;
+
+    updateScore(roundsPlayed, humanScore, computerScore);
+
+    if (roundsPlayed == maxRoundCount) {
+        const finalResult = document.querySelector("#header");    
+        if (humanScore > computerScore) {
+            finalResult.style.color = "green";
+            finalResult.textContent = "You win!";
+        }
+        else if (humanScore == computerScore) {
+            finalResult.style.color = "blue";
+            finalResult.textContent = "It's a tie";
+        }
+        else {
+            finalResult.style.color = "red";
+            finalResult.textContent = "You lose!";
+        }
+    }
 }
 
-function playGame() {
-    while (roundsPlayed < 5) {
-        playRound(getHumanChoice(), getComputerChoice());   
-    }
-    if (humanScore > computerScore) {
-        console.log("You win!");
-    }
-    else if (humanScore == computerScore) {
-        console.log("It's a tie");
-    }
-    else {
-        console.log("You lose!");
-    }
-}
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
 
-playGame();
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    if (roundsPlayed < maxRoundCount) {
+    playRound(getHumanChoice(button.textContent), getComputerChoice());
+    }
+  });
+});
+
+function updateScore(roundsPlayed, humanScore, computerScore) {
+    const rounds = document.querySelector("#rounds-played");
+    rounds.textContent = `Rounds played: ${roundsPlayed}`;
+
+    const human = document.querySelector("#user-score");
+    human.textContent = `Human Score: ${humanScore}`;
+
+    const pc = document.querySelector("#pc-score");
+    pc.textContent = `Computer Score: ${computerScore}`;
+}
